df05d20c56f2117a27647f1b2fb60cf0d319e988
allenbra17 <allenbra17@gmail.com>
2021-12-06 12:08:03 -0500

    initial js

---
+++ codingFoundations/javascriptProjects/javascriptBasics/comments.js
@@ -0,0 +1,10 @@
+This is a single line Comment, I am ignored.
+
+console.log("running javascript..."); // only I am a comment in this line.
+
+/*
+I am a
+multi-line
+comment.
+
+*/
---
+++ codingFoundations/javascriptProjects/javascriptBasics/variables.js
@@ -0,0 +1,24 @@
+// Javascript Variable Declaration
+var ofIndependence;
+
+// A variable is a NAMED box that holds something in JavaScript
+console.log(ofIndependence); // Shows as undefined until initialized
+
+// Initialize the variable
+ofIndependence="1,2,3";
+
+console.log(ofIndependence); // Shows as 1,2,3 now that its initialized
+
+ofIndependence="4,5,6";
+console.log(ofIndependence); //Shows as 4,5,6 now that its changed and shows all 3 sets of variables
+
+//Declare and initialize
+var food = "corn";
+console.log(food)
+
+//other types of variables
+let favDay = "Halloween";
+console.log(favDay)
+
+const tax = .07
+console.log(tax)
